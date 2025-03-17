a1ad743a268c45d6068e60644246574568c0c0a1
louis-stintzy <louis.stintzy@gmail.com>
2025-03-17 13:53:10 +0000

    feat(auth): import the auth.js file provided in the exercise to simulate data persistence via localStorage

--- src/@types/auth.d.ts
+++ src/@types/auth.d.ts
@@ -8,3 +8,11 @@ export interface LoginCredentials {
   email: string;
   password: string;
 }
+
+export interface UserData {
+  id: string;
+  nickname: string;
+  email: string;
+  password: string;
+  isLogged: boolean;
+}
