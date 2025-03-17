174fe7bb61188fc156a4e8bd4f51a71e9a4b23b2
louis-stintzy <louis.stintzy@gmail.com>
2025-03-17 13:13:38 +0000

    feat(forms): RegisterForm

--- src/@types/auth.d.ts
+++ src/@types/auth.d.ts
@@ -1,3 +1,9 @@
+export interface RegisterCredentials {
+  pseudo: string;
+  email: string;
+  password: string;
+}
+
 export interface LoginCredentials {
   email: string;
   password: string;
---
+++ src/components/PageComponents/RegisterForm/registerForm.schema.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+export const registerFormSchema = z.object({
+  pseudo: z
+    .string()
+    .min(3, 'Le pseudo doit contenir au moins 3 caractères')
+    .max(50, 'Le pseudo doit contenir au plus 50 caractères'),
+  email: z.string().email('Adresse email invalide'),
+  password: z
+    .string()
+    .min(12, 'Le mot de passe doit contenir au moins 12 caractères')
+    .max(100, 'Le mot de passe doit contenir au plus 100 caractères')
+    .regex(
+      /[A-Z]/,
+      'Le mot de passe doit contenir au moins une lettre majuscule'
+    )
+    .regex(
+      /[a-z]/,
+      'Le mot de passe doit contenir au moins une lettre minuscule'
+    )
+    .regex(/\d/, 'Le mot de passe doit contenir au moins un chiffre')
+    .regex(
+      /[@$!%*?&]/,
+      'Le mot de passe doit contenir au moins un caractère spécial'
+    ),
+});
