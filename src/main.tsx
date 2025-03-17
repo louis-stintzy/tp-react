8b5dcaae236e31d2ebc509701a89416ce55d25f0
louis-stintzy <louis.stintzy@gmail.com>
2025-03-17 09:07:20 +0000

    style: reset css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
 
+import './reset.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
